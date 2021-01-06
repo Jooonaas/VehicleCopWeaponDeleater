ce07a19be5e19af35a821c362c96e238532941c7
Jonas <jonas.lehwald@web.de>
2021-01-06 16:51:02 +0100

    first commit

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'disable the giving of weapon when you join a police vehicle'
+
+version '1.0.0'
+name 'VehicleCopWeaponDeleater'
+
+client_scripts {
+	'client/client.lua'
+}
